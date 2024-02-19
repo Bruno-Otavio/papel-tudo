9b4cd51a6eb90215903c3ec52c57a2cf5c55aec6
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-02-18 22:36:12 -0300

    Added renderItems

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,39 @@
-const itemTable = document.querySelector("#items-table");
+const itemData = document.querySelector("#table-data");
 const form = document.querySelector("#form-new-item");
 
 const backendUrl = "http://localhost:3000";
 
+window.onload = () => {
+	renderItems(itemData);
+}
+
+async function renderItems(element) {
+	const data = await fetch(`${backendUrl}/items`)
+		.then(res => res.json());
+	
+	data.forEach((item) => {
+		element.innerHTML += `
+			<td>${item.id}</td>
+			<td>${item.nome}</td>
+			<td>${item.descricao}</td>
+			<td>${item.valor}</td>
+		`;
+	});
+}
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	const data = new FormData(event.target);
+	const data = new FormData(form);
+
+	fetch(`${backendUrl}/items/create`, {
+		method: "POST",
+		body: data,
+		headers: new Headers({
+			Accept: 'application/json',
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin":'http://localhost:3000/items/create',
+			"Access-Control-Allow-Methods": "POST"
+		})
+	});
 })
 
-async function renderItems() {
-	const data = await fetch(`${backendUrl}/items`).then(res.josn());
-	console.log(data);
-}
